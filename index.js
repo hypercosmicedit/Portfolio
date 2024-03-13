d8f9e76508939b2e02d29901825812152f025855
sarthak <sarthak613singh@gmail.com>
2024-03-13 13:03:37 +0530

    complete responsive button left

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const observer = new IntersectionObserver((entries =>{
     entries.forEach((entry)=>{
-        console.log(entry)
         if(entry.isIntersecting){
             entry.target.classList.add('show');
         }else{
@@ -8,7 +7,5 @@ const observer = new IntersectionObserver((entries =>{
         }
     })
 }))
-
-
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
