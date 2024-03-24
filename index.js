f8e5c58c807af7bd92e2e7f78b901b43deed319d
sarthak <sarthak613singh@gmail.com>
2024-03-25 02:15:10 +0530

    Home Page Complete

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ const observer = new IntersectionObserver((entries =>{
 }))
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
+
+
+const aboutMe = document.querySelector("#aboutBtn");
+aboutMe.addEventListener("click", ()=>{ 
+    document.body.innerHTML = "";
+    let el = document.createElement("div");
+    el.add.classList("navBarHome");
+    el.innerHTML = "Hi";
+    document.body.appendChild(el);
+})
+
+const contactUs = document.querySelector("#contactBtn");
+contactUs.addEventListener("click", ()=>{ 
+    document.body.innerHTML = "";
+    let el = document.createElement("div");
+    el.innerHTML = "<h1>hii</h1>";
+    document.body.appendChild(el);
+})
