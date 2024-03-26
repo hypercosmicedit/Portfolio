cf1de056fc7be634eaa5a9aca0db23ef373d871a
sarthak <sarthak613singh@gmail.com>
2024-03-26 15:41:08 +0530

    Changes

--- index.js
+++ index.js
@@ -11,19 +11,62 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
 
 
-const aboutMe = document.querySelector("#aboutBtn");
+
+
+
+//*********About Me Page*********//
+const aboutMe = document.querySelector(".aboutBtn");
 aboutMe.addEventListener("click", ()=>{ 
     document.body.innerHTML = "";
     let el = document.createElement("div");
-    el.add.classList("navBarHome");
-    el.innerHTML = "Hi";
+        let name = document.createElement("div");
+        let navigation = document.createElement("div");
+        navigation.classList.add("grpBtn");
+
+
+        let homeBtn = document.createElement("button")
+        homeBtn.innerHTML = "<a href='./index.html'>HOME</a>";
+        homeBtn.classList.add("navigationBarAbout");
+        homeBtn.classList.add("aboutBtn");
+
+        let AboutBtn = document.createElement("button")
+        AboutBtn.innerText = "RESUME";
+        AboutBtn.classList.add("navigationBarAbout");
+        AboutBtn.classList.add("aboutBtn");
+
+        let ContactBtn = document.createElement("button")
+        ContactBtn.innerText = "CONTACT US";
+        ContactBtn.classList.add("navigationBarAbout");
+        ContactBtn.classList.add("contactBtn");
+
+
+        navigation.appendChild(homeBtn);
+        navigation.appendChild(AboutBtn);
+        navigation.appendChild(ContactBtn);
+
+
+        name.innerText = "HYPER COSMIC EDITS";
+        name.classList.add("nameHome")
+
+    el.classList.add("navBarHome");
+    el.appendChild(name);
+    el.appendChild(navigation);
     document.body.appendChild(el);
 })
 
-const contactUs = document.querySelector("#contactBtn");
+
+
+//*********Contact Us ***********//
+const contactUs = document.querySelector(".contactBtn");
 contactUs.addEventListener("click", ()=>{ 
     document.body.innerHTML = "";
     let el = document.createElement("div");
     el.innerHTML = "<h1>hii</h1>";
     document.body.appendChild(el);
+})
+
+
+const homePage = document.querySelector(".homeBtn");
+contactUs.addEventListener("click", ()=>{ 
+    window.location.href = "index.html";
 })
