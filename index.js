2e59d188b1200cae622741a800c7ee92e47a78c0
sarthak <sarthak613singh@gmail.com>
2024-04-06 13:05:43 +0530

    Update navigation bar styling and add form submission functionality

--- index.js
+++ index.js
@@ -11,17 +11,32 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
 
 
+const form = document.getElementById('contactForm');
 
-const container = document.querySelector(".firstHalf");
-const container1 = document.querySelector(".secondHalf");
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    console.log(name, email, message);
 
-container.addEventListener('mouseenter', (e)=>{
-    console.log(e);
-    container1.style.overflow = 'hidden';
-    console.log("hello");
-});
-
-
-
-
-
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "sarthak613singh@gmail.com",
+        Password : "B9966F82772B8FA4E224D213642C35F83909",
+        To : "mits1451@gmail.com",
+        From : "sarthak613singh@gmail.com",
+        Subject : "You have a new message from your website from " + name,
+        Body : email + message
+    }).then(
+        message => {
+            console.log(message);
+            alert('Message sent successfully');
+            form.reset();
+        },
+        error => {
+            console.error('Error:', error);
+            alert('Failed to send message. Please try again later.');
+        }
+    );
+})
