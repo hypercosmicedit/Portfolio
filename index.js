eebbeb6234f2fe98016c504152777cb468c225a1
sarthak <sarthak613singh@gmail.com>
2024-04-06 14:36:47 +0530

    Update contact page styling and add form submission functionality

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const observer = new IntersectionObserver((entries =>{
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
 
-
+       
 const form = document.getElementById('contactForm');
 
 form.addEventListener('submit', (e)=>{
@@ -18,25 +18,28 @@ form.addEventListener('submit', (e)=>{
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
-    console.log(name, email, message);
+
+    const body = `Customer Name:  ${name};   
+
+    Customer Email:   ${email} (Click on the mail to chat with customer);
+
+    Customer Message:    ${message}`;
 
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "sarthak613singh@gmail.com",
-        Password : "B9966F82772B8FA4E224D213642C35F83909",
-        To : "mits1451@gmail.com",
+        Password : "15848CCE757683C9357DFA21AC22377CD216",
+        To : "hypercosmic.edit@gmail.com",
         From : "sarthak613singh@gmail.com",
-        Subject : "You have a new message from your website from " + name,
-        Body : email + message
+        Subject : "You have a new message from your website (Hyper Cosmic Edits) from "+ name,
+        Body : body
     }).then(
         message => {
-            console.log(message);
             alert('Message sent successfully');
             form.reset();
         },
         error => {
-            console.error('Error:', error);
             alert('Failed to send message. Please try again later.');
         }
     );
-})
+});
